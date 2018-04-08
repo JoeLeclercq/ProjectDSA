42e2dd68e3bdb863419836a828197023f93b851e
Leclercq <leclercqj8@elvix2.rowan.edu>
2018-04-08 19:01:00 -0400

    Beginning Driver

--- src/Driver.java
+++ src/Driver.java
@@ -5,17 +5,106 @@ public class Driver{
     private static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String args[]){
         Restaurant restaurant = new Restaurant();
-        String name;
-        int seats;
+        String in;
         int tables;
         System.out.println("Enter your restaurant configuration:");
-        System.out.print("\nHow many tables does your pet-friendly section have? ");
-        tables = Integer.parseInt(stdin.readLine());
+        tables = IOTools.promptPositiveInteger("How many tables does your pet-friendly section have? ");
         for(int i = 0; i < tables; i++){
-            System.out.print("\nEnter table name: ");
-            name = stdin.readLine();
-            System.out.print("\nEnter number of seats: ");
-            seats = Integer.parseInt(stdin.readLine());
-        }
+            addTable(restaurant);
+        }  
+        tables = IOTools.promptPositiveInteger("How many tables does your non- pet-friendly section have? ");
+        for(int i = 0; i < tables; i++){
+            addTable(restaurant);
+        }  
+        System.out.println("Select from the following menu: ");
+        System.out.println("Select from the following menu:" +
+                + "    0.	Close the restaurant." +
+                + "    1.	Customer party enters the restaurant." +
+                + "    2.	Customer party is seated and served." +
+                + "    3.	Customer party leaves the restaurant." + 
+                + "    4.	Add a table." +
+                + "    5.	Remove a table." + 
+                + "    6.	Display available tables." +
+                + "    7.	Display info about waiting customer parties." + 
+                + "    8.	Display info about customer parties being served.");
+        in = IOTools.promptLine("Make your menu selection now: ");
+        switch (in){
+            case "0":
+                System.exit();
+                break;
+            case "1":
+                addParty(restaurant);
+                break;
+            case "2":
+                seatParty(restaurant);
+                break;
+            case "3":
+                partyLeaves(restaurant);
+                break;
+            case "4":
+                addTable(restaurant);
+                break;
+            case "5":
+                removeTable(restaurant);
+                break;
+            case "6":
+                displayAvailableTables(restaurant);
+                break;
+            case "7":
+                displayPartyWait(restaurant);
+                break;
+            case "8":
+                displayPartyServed(restaurant);
+                break;
+            default:
+                System.out.println("Not an acceptable menu selection");
+        }        
     }   
+
+    public static void addParty(Restaurant restaurant){
+        String name;
+        int size;
+        boolean pet;
+        name = IOTools.promptLine("Enter customer name : ");
+        while(!restaurant.partyNameFree(name)){
+            System.out.println("There already exists a customer with this name in the restaurant.\n\tPlease select another name.");
+            name = IOTools.promptLine("Enter customer name : ");
+        }
+        size = IOTools.promptPositiveInteger("Enter number of seats for customer " + name + ": ");
+        pet = IOTools.promptYesOrNo("Does your party have pets?");
+        if(pet){
+            restaurant.partyEnters(new PetParty(name, size));
+        }
+        else{
+            restaurant.partyEnters(new NoPetParty(name, size));
+        }
+    }
+
+    public static void seatParty(Restaurant restaurant){
+        
+    }
+
+    public static void partyLeaves(Restaurant restaurant){
+        
+    }
+//table and boolean
+    public static void addTable(Restaurant restaurant){
+        
+    }
+
+    public static void removeTable(Restaurant restaurant){
+        
+    }
+
+    public static void displyAvailableTables(Restaurant restaurant){
+        
+    }
+   
+     public static void displayPartyWait(Restaurant restaurant){
+        
+    }
+    
+    public static void displayPartyServed(Restaurant restaurant){
+        
+    }
 }   
