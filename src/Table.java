506394bfdeb2818f93f6b910cba099445daf4227
bchesko <6621861+bchesko@users.noreply.github.com>
2018-04-08 17:20:15 -0400

    overwrite test

---
+++ src/Table.java
@@ -0,0 +1,23 @@
+public class Table{
+    private int size;
+    private String name;
+    private AbstractParty party;
+    public Table(String name, int size){
+        this.name = name;
+        this.size = size;
+        party = null;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getSize(){
+        return size;
+    }
+    public AbstractParty getParty(){
+        return party;
+    }
+    public void setParty(AbstractParty party){
+        this.party = party;
+    }
+
+}
