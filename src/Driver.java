01efa6732f2414a6dd7affad7ddf2b4c1f6c5506
Leclercq <leclercqj8@elvix2.rowan.edu>
2018-04-14 16:36:56 -0400

    added more to Driver

--- src/Driver.java
+++ src/Driver.java
@@ -10,55 +10,56 @@ public class Driver{
         System.out.println("Enter your restaurant configuration:");
         tables = IOTools.promptPositiveInteger("How many tables does your pet-friendly section have? ");
         for(int i = 0; i < tables; i++){
-            addTable(restaurant);
+            addTable(restaurant, true);
         }  
-        tables = IOTools.promptPositiveInteger("How many tables does your non- pet-friendly section have? ");
+        tables = IOTools.promptPositiveInteger("How many tables does your non-pet-friendly section have? ");
         for(int i = 0; i < tables; i++){
-            addTable(restaurant);
+            addTable(restaurant, false);
         }  
-        System.out.println("Select from the following menu: ");
         System.out.println("Select from the following menu:" +
-                + "    0.	Close the restaurant." +
-                + "    1.	Customer party enters the restaurant." +
-                + "    2.	Customer party is seated and served." +
-                + "    3.	Customer party leaves the restaurant." + 
-                + "    4.	Add a table." +
-                + "    5.	Remove a table." + 
-                + "    6.	Display available tables." +
-                + "    7.	Display info about waiting customer parties." + 
-                + "    8.	Display info about customer parties being served.");
-        in = IOTools.promptLine("Make your menu selection now: ");
-        switch (in){
-            case "0":
-                System.exit();
-                break;
-            case "1":
-                addParty(restaurant);
-                break;
-            case "2":
-                seatParty(restaurant);
-                break;
-            case "3":
-                partyLeaves(restaurant);
-                break;
-            case "4":
-                addTable(restaurant);
-                break;
-            case "5":
-                removeTable(restaurant);
-                break;
-            case "6":
-                displayAvailableTables(restaurant);
-                break;
-            case "7":
-                displayPartyWait(restaurant);
-                break;
-            case "8":
-                displayPartyServed(restaurant);
-                break;
-            default:
-                System.out.println("Not an acceptable menu selection");
-        }        
+                "    0.	Close the restaurant." +
+                "    1.	Customer party enters the restaurant." +
+                "    2.	Customer party is seated and served." +
+                "    3.	Customer party leaves the restaurant." + 
+                "    4.	Add a table." +
+                "    5.	Remove a table." + 
+                "    6.	Display available tables." +
+                "    7.	Display info about waiting customer parties." + 
+                "    8.	Display info about customer parties being served.");
+        do{
+            in = IOTools.promptLine("Make your menu selection now: ");
+            switch (in){
+                case "0":
+                    System.exit();
+                    break;
+                case "1":
+                    addParty(restaurant);
+                    break;
+                case "2":
+                    seatParty(restaurant);
+                    break;
+                case "3":
+                    partyLeaves(restaurant);
+                    break;
+                case "4":
+                    addTable(restaurant);
+                    break;
+                case "5":
+                    removeTable(restaurant);
+                    break;
+                case "6":
+                    displayAvailableTables(restaurant);
+                    break;
+                case "7":
+                    displayPartyWait(restaurant);
+                    break;
+                case "8":
+                    displayPartyServed(restaurant);
+                    break;
+                default:
+                    System.out.println("Not an acceptable menu selection");
+            }  
+        }while(true);      
     }   
 
     public static void addParty(Restaurant restaurant){
@@ -85,34 +86,42 @@ public class Driver{
     }
 
     public static void partyLeaves(Restaurant restaurant){
-        Table table;
+        Object[] exitInfo;
         String party = IOTools.promptLine("Enter the name of the customer that wants to leave: ");
-        table = restaurant.partyExits(party);
+        exitInfo = restaurant.partyExits(party);
         if(table != null){
-            
+            System.out.println("Table " + exitInfo[0].getName() + " with " + exitInfo[0].getSize() + " has been freed.");
+            System.out.println("Customer " + party + " party of " + exitInfo[1].getSize() + "(" +(exitInfo[1] instanceof PetParty?"Pet Party":"No Pet") + " is leaving the restaurant");
         }
         else{
-            
+            System.out.println("The party " + party + " is not seated currently");
         }
     }
-//table and boolean
-    public static void addTable(Restaurant restaurant){
-        
+    //table and boolean
+    public static void addTable(Restaurant restaurant, boolean pet){
+        boolean isDone = false;
+        String name;
+        while(!isDone){
+            name = IOTools.promptLine("Enter the name of the table: ");
+            isDone = tableNameFree(name);
+        }     
+        int size = IOTolls.promptPositiveInteger("How many seats are at this table?");
+        restaurant.addTable(new Table(name, size), pet);
     }
 
     public static void removeTable(Restaurant restaurant){
-        
+
     }
 
     public static void displyAvailableTables(Restaurant restaurant){
-        
+
     }
-   
-     public static void displayPartyWait(Restaurant restaurant){
-        
+
+    public static void displayPartyWait(Restaurant restaurant){
+
     }
-    
+
     public static void displayPartyServed(Restaurant restaurant){
-        
+
     }
 }   
