00183aec36f894dc62fb1047f40fa213cf6966f2
Joseph Leclercq <leclercqj8@elvix2.rowan.edu>
2018-04-18 20:12:15 -0400

    Driver header added

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,13 @@
+/*
+ * Purpose: Data Structure and Algorithms Project
+ * Status: Complete and thoroughly tested
+ * Last update: 04/18/18
+ * Submitted:  04/18/18
+ * Comment: test suite and sample run attached
+ * @author: Brian Chesko and Joseph Leclercq
+ * @version: 2018.04.18
+ */
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
